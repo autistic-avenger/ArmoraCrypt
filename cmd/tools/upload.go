44ef9941abf9eec5d9ee650889e8ae3eabe7b987
Aditya Kumar <aditya4u164@gmail.com>
2026-03-03 03:29:14 +0530

    FIXED

--- cmd/tools/upload.go
+++ cmd/tools/upload.go
@@ -3,7 +3,7 @@ package tools
 import (
 	"armoracrypt/cmd"
 	"armoracrypt/internal"
-	dropboxapi "armoracrypt/internal/dropboxApi"
+	dropboxapi "armoracrypt/internal/dropboxapi"
 	"fmt"
 	"os"
 	"os/exec"
@@ -11,81 +11,77 @@ import (
 
 	"github.com/spf13/cobra"
 )
+
 var Upload = &cobra.Command{
-	Use: "upload",
+	Use:   "upload",
 	Short: "safe upload to cloud",
-	Long: "Encrypts and Uploads file/folder to the Cloud Storage",
-	Run:func(cmd *cobra.Command, args []string) {
+	Long:  "Encrypts and Uploads file/folder to the Cloud Storage",
+	Run: func(cmd *cobra.Command, args []string) {
 		internal.Keygen()
-		fp,err := cmd.Flags().GetString("fp")
-		if err!=nil{
+		fp, err := cmd.Flags().GetString("fp")
+		if err != nil {
 			fmt.Println("Error getting fp Flag")
 		}
-		Abs,_:= filepath.Abs(fp)
-		info ,err := os.Stat(Abs)
-		if err!=nil{
+		Abs, _ := filepath.Abs(fp)
+		info, err := os.Stat(Abs)
+		if err != nil {
 			fmt.Println("Incorrect PATH")
 			return
 		}
-		if info.IsDir(){
-			_,err := exec.Command("armoracrypt", "encrypt", "--d", Abs).Output()
-			if err!=nil{
-				fmt.Println("Error Uploading File")
-				return
-			}
-			token,err := internal.CheckToken()
-			if err!=nil{
-				fmt.Println(err)
-				return
-			}
-			fileInfo ,_ := os.Stat(Abs)
-			//aPi works:
-			//token ,relativeDROPPATH,localPATH
-
-			err = dropboxapi.UploadFile(token,"/ARMORA/FOLDERS/"+fileInfo.Name()+".zip.crypt",Abs+".zip.crypt")
-			if err!=nil{
-				fmt.Println("Error Uploading...",err)
-				return
-			}
-
-			fmt.Println("[UPLOADED SUCCESSFULLY]")
-			defer os.Remove(Abs+".zip.crypt")
-
-		}else{
-			_,err := exec.Command("armoracrypt","encrypt","--fp",Abs).Output()
-			if err!=nil{
+		if info.IsDir() {
+			_, err := exec.Command("armoracrypt", "encrypt", "--d", Abs).Output()
+			if err != nil {
 				fmt.Println("Error Uploading File")
 				return
 			}
 
-			token,err := internal.CheckToken()
-			if err!=nil{
+			token, err := internal.CheckToken()
+			if err != nil {
 				fmt.Println(err)
 				return
 			}
-			fileInfo ,_ := os.Stat(Abs)
-
+			fileInfo, _ := os.Stat(Abs)
 			//aPi works:
 			//token ,relativeDROPPATH,localPATH
-			err = dropboxapi.UploadFile(token,"/ARMORA/FILES/"+fileInfo.Name()+".crypt",Abs+".crypt")
-			if err!=nil{
-				fmt.Println("Error Uploading...",err)
+
+			err = dropboxapi.UploadFile(token, "/ARMORA/FOLDERS/"+fileInfo.Name()+".zip.crypt", Abs+".zip.crypt")
+			if err != nil {
+				fmt.Println("Error Uploading...", err)
 				return
 			}
 
-
-			
 			fmt.Println("[UPLOADED SUCCESSFULLY]")
-			defer os.Remove(Abs+".crypt")
+			defer os.Remove(Abs + ".zip.crypt")
+
+		} else {
+			_, err := exec.Command("armoracrypt", "encrypt", "--fp", Abs).Output()
+			if err != nil {
+				fmt.Println("Error Uploading File")
+				return
+			}
+
+			token, err := internal.CheckToken()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fileInfo, _ := os.Stat(Abs)
+
+			//aPi works:
+			//token ,relativeDROPPATH,localPATH
+			err = dropboxapi.UploadFile(token, "/ARMORA/FILES/"+fileInfo.Name()+".crypt", Abs+".crypt")
+			if err != nil {
+				fmt.Println("Error Uploading...", err)
+				return
+			}
+			fmt.Println("[UPLOADED SUCCESSFULLY]")
+			defer os.Remove(Abs + ".crypt")
 		}
 
-
-
-
 	},
-} 
+}
 
-func init(){
-	Upload.Flags().String("fp","/NarendraMODI/ISRAEL","Upload to cloud")
+func init() {
+	Upload.Flags().String("fp", "/NarendraMODI/ISRAEL", "Upload to cloud")
 	cmd.RootCmd.AddCommand(Upload)
 }
---
+++ internal/dropboxapi/uploadfile.go
@@ -0,0 +1,34 @@
+package dropboxapi
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func UploadFile(token string, dropboxPath string, localFilePath string) error {
+	fileData, err := os.ReadFile(localFilePath)
+
+	reqs, err := http.NewRequest(
+		"POST",
+		"https://content.dropboxapi.com/2/files/upload",
+		bytes.NewReader(fileData),
+	)
+	if err != nil {
+		return err
+	}
+
+	reqs.Header.Set("Authorization", "Bearer "+token)
+	reqs.Header.Set("Content-Type", "application/octet-stream")
+	reqs.Header.Set("Dropbox-API-Arg", fmt.Sprintf("{\"autorename\":false,\"mode\":\"overwrite\",\"mute\":false,\"path\":\"%s\",\"strict_conflict\":false}", dropboxPath))
+
+	client := &http.Client{}
+	resp, err := client.Do(reqs)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
